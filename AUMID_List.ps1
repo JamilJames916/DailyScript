eab5c51388bcc51f5e39dee379830790b4141558
JamilJames916 <jamiljames86@gmail.com>
2024-07-11 02:54:23 -0500

    List of apps
    List of pre-installed apps and their IDs, so that It can be used for a script to delete pre-installed apps, Pre- Install apps, unpin certain apps from the taksbar as well pinning certain apps to the taskbar.

---
+++ AUMID_List.ps1
@@ -0,0 +1,19 @@
+﻿# To get the names and AUMIDs for Microsoft Store apps installed for the current user.
+
+$installedapps = Get-AppxPackage
+
+$aumidList = @()
+foreach ($app in $installedapps)
+{
+    foreach ($id in (Get-AppxPackageManifest $app).package.applications.application.id)
+    {
+        $aumidList += $app.packagefamilyname + "!" + $id
+    }
+}
+
+$aumidList
+
+
+# You can add the -user <username> or the 
+# -allusers parameters to the Get-AppxPackage cmdlet to list AUMIDs for other users. 
+# You must use an elevated Windows PowerShell prompt to use the -user or -allusers parameters.
---
+++ AUMID_StartMenu.ps1
@@ -0,0 +1,20 @@
+﻿# The following code sample creates a function in Windows PowerShell
+# that returns the AUMID of any application currently listed in the Start menu.
+
+function Get-AppAUMID {
+param (
+[string]$AppName
+)
+$Apps = (New-Object -ComObject Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items()
+if ($AppName){
+    $Result = $Apps | Where-Object { $_.name -like "*$AppName*" } | Select-Object name,@{n="AUMID";e={$_.path}}
+        if ($Result){
+            Return $Result
+        }
+    else {"Unable to locate {0}" -f $AppName}
+}
+else {
+    $Result = $Apps | Select-Object name,@{n="AUMID";e={$_.path}}
+    Return $Result
+}
+}
